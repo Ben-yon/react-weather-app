43f665c99bd46896c693fd7eb5102a4e52af59ac
Benjamin Ikeh <b.ikeh@yahoo.com>
2022-08-01 09:28:18 +0000

    consume cities api and inject results into the search bar

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4bffea99dfmshf7b157421c43d7fp192606jsn2a2e566dfb13',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
